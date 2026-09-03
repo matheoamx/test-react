0273b3bd97a19c8f7eefa3bcca87af88fe57562d
matheo <matheo.amouroux@ynov.com>
2026-09-03 17:26:50 +0200

    feat: passage des props pokemon dans App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import "./App.css";
-  import PokemonCard from "./components/PokemonCard";
+import PokemonCard from "./components/PokemonCard";
 
-  function App() {
-    return (
-      <div>
-        <PokemonCard />
-      </div>
-    );
-  }
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
-  export default App;
+function App() {
+  return (
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
+    </div>
+  );
+}
+
+export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,25 +1,14 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[1]; // <-- On passe à l'index 1 (Mew)
+function PokemonCard({ pokemon }) {
+  const { name, imgSrc } = pokemon;
 
   return (
     <figure>
-      {pokemon.imgSrc !== undefined ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      {imgSrc !== undefined ? (
+        <img src={imgSrc} alt={name} />
       ) : (
         <p>???</p>
       )}
-      <figcaption>{pokemon.name}</figcaption>
+      <figcaption>{name}</figcaption>
     </figure>
   );
 }
