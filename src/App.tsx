293851f52febdb836280255bcd7000d9d833941b
matheo <matheo.amouroux@ynov.com>
2026-09-04 16:55:32 +0200

    ajout de tous les pokemons et validation de la liste dynamique

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,21 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
@@ -24,9 +39,17 @@ function App() {
 
   return (
     <div>
+      <nav>
+        {pokemonList.map((element) => (
+          <button
+            key={element.name}
+            onClick={() => setPokemonName(element.name)}
+          >
+            {element.name}
+          </button>
+        ))}
+      </nav>
       <PokemonCard pokemon={pokemon} />
-      <button onClick={() => setPokemonName("bulbasaur")}>bulbasaur</button>
-      <button onClick={() => setPokemonName("mew")}>mew</button>
     </div>
   );
 }
