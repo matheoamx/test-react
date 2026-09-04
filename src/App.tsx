513097994aac046358d132517ffa70bed3a5f46a
matheo <matheo.amouroux@ynov.com>
2026-09-04 15:12:32 +0200

    typage des props avec typescript et configuration de tsconfig.app.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,11 @@
-function PokemonCard({ pokemon }) {
+interface PokemonCardProps {
+  pokemon: {
+    name: string;
+    imgSrc?: string;
+  };
+}
+
+function PokemonCard({ pokemon }: PokemonCardProps) {
   const { name, imgSrc } = pokemon;
 
   return (
